2272ef0b5b0d9203472cde02b30bdae7b0cd339d
macluiggy <ferrinluiggy@gmail.com>
2021-09-28 14:38:30 -0500

    jdjjd

--- myApp.js
+++ myApp.js
@@ -3,7 +3,6 @@ var mongoose =  require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 let Person;
 
 const createAndSavePerson = (done) => {
