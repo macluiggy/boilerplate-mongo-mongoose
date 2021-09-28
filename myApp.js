911f277222a3cdea358cc9066ea9fe1eda9b1a08
macluiggy <ferrinluiggy@gmail.com>
2021-09-28 14:47:36 -0500

    importing env file

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: './sample.env'});
 var mongoose =  require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
